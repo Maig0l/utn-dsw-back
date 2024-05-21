779f7fc18739970a4de12ea1d0007c08b523ffef
Miguel Cabanellas <miguel.cabanellas03@gmail.com>
2024-05-21 16:09:16 -0300

    Terminar primer GET listado de Tiendas de la API

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 import express from 'express'
+import {Shop} from './Shop.js'
 
 const app = express()
 
+const shops: Shop[] = [];
+shops.push(new Shop("Steam", "/assets/steam.svg", "https://steampowered.com/"))
+shops.push(new Shop("Epic Games", "/assets/epic.svg", "https://store.epicgames.com/"))
+shops.push(new Shop("PSN Store", "/assets/psn.svg", "https://store.playstation.com/"))
+
+app.get("/api/shops", (req, res) => {
+  res.json(shops)
+})
+
+
 // Responder a la ruta / con la callback
 app.use('/', (req, res) => {
-  switch (req.method) {
-    case 'GET':
-      res.json('Create tienda')
-      break
-    case 'POST':
-      res.json('update tienda')
-      break
-    default:
-      res.send('No deberias estar aqui')
-  }
-  res.json({"health": "alive (barely)"})
+  res.send(`Entidad: Tienda <br/>
+  <ul>
+    <li><a href="/api/shops">GET tiendas</a> (precargadas y hardcodeadas)</li>
+  </ul>`)
 })
 
 app.listen(8080, ()=> {
