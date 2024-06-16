3de2bf7104746849705f7ff7e2f135e35107cdc0
Matias <matiasanin15@gmail.com>
2024-06-16 14:47:26 -0300

    initial tag-CRUD commit

---
+++ src/Tag.ts
@@ -0,0 +1,32 @@
+export class Tag {
+    private static ID_COUNTER =0;
+    private id: number;
+
+    constructor(
+        private name: string,
+        private description: string,
+
+    )
+    { this.id = ++Tag.ID_COUNTER;
+        this.name = name;
+        this.description = description 
+
+    }
+
+
+    getId(): number{
+        return this.id;
+
+
+    }
+
+    getName(): string{
+        return this.name;
+
+    }
+
+    getDescriptions(): string{
+        return this.description;
+    }
+}
+
