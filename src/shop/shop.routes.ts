bb752f3f2d93b40dc9c631106c511551735cfdeb
Miguel Cabanellas <miguel.cabanellas03@gmail.com>
2024-06-27 16:10:05 -0300

    Minor: Align routes

--- src/shop/shop.routes.ts
+++ src/shop/shop.routes.ts
@@ -11,6 +11,6 @@ shopRouter.get('/', findAll)
 shopRouter.post('/', sanitizeInput, add)
 
 shopRouter.get('/:id', validateExists, findOne)
-shopRouter.delete('/:id', validateExists, remove)
-shopRouter.patch('/:id', validateExists, sanitizeInput, update)
 shopRouter.put('/:id', validateExists, sanitizeInput, update)
+shopRouter.patch('/:id', validateExists, sanitizeInput, update)
+shopRouter.delete('/:id', validateExists, remove)
