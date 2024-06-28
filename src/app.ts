f9dd3a11bbae9559dee0da1c3bb38fcf756db409
Miguel Cabanellas <miguel.cabanellas03@gmail.com>
2024-06-28 06:10:46 -0300

    Remove index.html

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,6 @@ import { shopRouter } from './shop/shop.routes.js';
 import { studioRouter } from './studio/studio.routes.js';
 import { userRouter } from './user/user.routes.js';
 
-// Metadatos para saber en qué directorio está el programa
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
-
-// La raíz del proyecto es uno arriba pues pnpm ejecuta el app.js dentro de la carpeta dist/
-const ROOT = path.join(__dirname, '..')
 const app = express()
 
 // Tenemos que usar el middleware url-encoded extendido porque Express
@@ -20,7 +14,6 @@ const app = express()
 // Nota: En Postman usar body de tipo x-www-form-urlencoded
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-app.use(express.static("public"))
 
 // Registrar routers para entidades
 app.use('/api/users', userRouter)
@@ -28,22 +21,10 @@ app.use('/api/shops', shopRouter)
 app.use('/api/studios', studioRouter)
 app.use('/api/platforms', platformRouter)
 
-// Index para debug
-app.get('/', (req, res) => {
-  res.sendFile("index.html", {root: ROOT})
-})
-
-app.post('/restart', (req, res) => {
-  console.log("Restarting server...")
-  res.json({message: "Restarting server..."})
-  fs.writeFileSync("src/restart.ts", `const x = "${new Date().toISOString()}"`)
-})
-
 app.use((_, res) => {
   return res.status(404).json({message: "Resource not found"})
 })
 
 app.listen(8080, ()=> {
-  console.log(`running on http://localhost:8080
-root in ${ROOT}`)
+  console.log(`Running on http://localhost:8080`)
 })
