a54976613d9646fbe5e10524aeb0b077aae16b83
Miguel Cabanellas <miguel.cabanellas03@gmail.com>
2024-07-30 17:41:19 -0300

    feat: add review router

---
+++ src/review/review.entity.ts
@@ -0,0 +1,28 @@
+import { ManyToMany, ManyToOne, OneToMany, Property, Collection, DateType } from "@mikro-orm/core";
+import { BaseEntity } from "../shared/db/baseEntity.entity.js";
+import { Game } from "../game/game.entity.js";
+import { Tag } from "../tag/tag.entity.js";
+import { User } from "../user/user.entity.js";
+
+export class Review extends BaseEntity {
+  @ManyToOne()
+  author!: User
+
+  @ManyToOne()
+  game!: Game
+
+  @Property()
+  score!: number
+
+  @Property({nullable: true})
+  title!: string
+
+  @Property({nullable: true})
+  body!: string
+
+  @ManyToMany('Tag', undefined, {nullable: true})
+  suggestedTags = new Collection<Tag>(this)
+
+  @Property()
+  createdAt: Date = new Date();
+}
