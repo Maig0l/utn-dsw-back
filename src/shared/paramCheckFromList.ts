43834a917479a64d28af85666dbe80c3f19d7cac
Miguel Cabanellas <miguel.cabanellas03@gmail.com>
2024-10-04 16:38:59 -0300

    refactor: Remove Repository interface and deprecate paramCheck
    paramCheckFromList should be replaced with Valibot schemas

--- src/shared/paramCheckFromList.ts
+++ src/shared/paramCheckFromList.ts
@@ -1,8 +1,10 @@
-import {Request} from 'express'
+import { Request } from 'express'
 
 // TODO: Volar esto a la mierda. Fue una mala idea (ver user.repository.ts)
 
-/** Toma los parámetros válidos (admitidos para post/put/patch) y genera una
+/** DEPRECADO: NO USAR!! USAR MEJOR ESQUEMAS DE Valibot
+ * 
+ * Toma los parámetros válidos (admitidos para post/put/patch) y genera una
  * función que chequea si el objeto (por ejemplo, request.body)tiene los
  * atributos necesarios
  * 
@@ -14,7 +16,7 @@ export function paramCheckFromList(attrs: string[]) {
   return (obj: any, all: boolean) => {
     const ATTRS = attrs
 
-    let f = (e: string) => {return Object.keys(obj).includes(e)}
+    let f = (e: string) => { return Object.keys(obj).includes(e) }
     if (all)
       return ATTRS.every(f)
     else
