2585444bc0308c87fd51acca88249f99011d602f
LuchoCsd <lucianocasado99@gmail.com>
2025-02-28 23:06:37 -0300

    playlist by user

--- src/playlist/playlist.routes.ts
+++ src/playlist/playlist.routes.ts
@@ -1,12 +1,22 @@
-import { Router } from "express";
-import { findOne, findAll, add, remove, update, validateExists,
-  sanitizeInput} from "./playlist.controller.js";
+import { Router } from 'express';
+import {
+  findOne,
+  findAll,
+  add,
+  remove,
+  update,
+  validateExists,
+  sanitizeInput,
+  getPlaylistsByOwner,
+} from './playlist.controller.js';
+import { get } from 'http';
 
-export const playlistRouter = Router()
+export const playlistRouter = Router();
 
-playlistRouter.get('/', findAll)
-playlistRouter.post('/', sanitizeInput, add)
-playlistRouter.get('/:id', validateExists, findOne)
-playlistRouter.put('/:id', validateExists, sanitizeInput, update)
-playlistRouter.patch('/:id', validateExists, sanitizeInput, update)
-playlistRouter.delete('/:id', validateExists, remove)
+playlistRouter.get('/search', getPlaylistsByOwner);
+playlistRouter.get('/', findAll);
+playlistRouter.post('/', sanitizeInput, add);
+playlistRouter.get('/:id', validateExists, findOne);
+playlistRouter.put('/:id', validateExists, sanitizeInput, update);
+playlistRouter.patch('/:id', validateExists, sanitizeInput, update);
+playlistRouter.delete('/:id', validateExists, remove);
