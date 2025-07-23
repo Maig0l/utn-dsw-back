37041b552ad0cee62ffbce69c6651e3e9805d7dd
LuchoCsd <lucianocasado99@gmail.com>
2025-07-23 00:04:55 -0300

    fix: standardize string quotes and improve platform entity and controller logic

--- src/platform/platform.routes.ts
+++ src/platform/platform.routes.ts
@@ -1,21 +1,24 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
   findOne,
   findAll,
   add,
   remove,
   update,
+  uploadImage,
   validateExists,
   sanitizeInput,
   findPlatformsByName,
-} from "./platform.controller.js";
+} from './platform.controller.js';
+import { upload } from '../shared/multer.js';
 
 export const platformRouter = Router();
 
-platformRouter.get("/search", findPlatformsByName);
-platformRouter.get("/", findAll);
-platformRouter.post("/", sanitizeInput, add);
-platformRouter.get("/:id", validateExists, findOne);
-platformRouter.put("/:id", validateExists, sanitizeInput, update);
-platformRouter.patch("/:id", validateExists, sanitizeInput, update);
-platformRouter.delete("/:id", validateExists, remove);
+platformRouter.get('/search', findPlatformsByName);
+platformRouter.get('/', findAll);
+platformRouter.post('/', sanitizeInput, add);
+platformRouter.get('/:id', validateExists, findOne);
+platformRouter.put('/:id', validateExists, sanitizeInput, update);
+platformRouter.patch('/:id', validateExists, sanitizeInput, update);
+platformRouter.patch('/:id/upload', validateExists, upload.single('img'), uploadImage);
+platformRouter.delete('/:id', validateExists, remove);
--- src/platform/platform.schema.ts
+++ src/platform/platform.schema.ts
@@ -1,19 +1,19 @@
-import * as v from "valibot";
+import * as v from 'valibot';
 
 const [NAME_LEN_MIN, NAME_LEN_MAX] = [1, 100];
 const ERR_NAME_LEN = `Name must be between ${NAME_LEN_MIN} and ${NAME_LEN_MAX} characters long`;
 const ERR_BAD_NAME =
-  "Invalid characters in playlist name. It can alphanumeric characters and dots.";
-const ERR_BAD_IMG = "Invalid image URL";
+  'Invalid characters in playlist name. It can alphanumeric characters and dots.';
+const ERR_BAD_IMG = 'Invalid image URL';
 
 const name = v.pipe(
   v.string(),
   v.minLength(NAME_LEN_MIN, ERR_NAME_LEN),
   v.maxLength(NAME_LEN_MAX, ERR_NAME_LEN),
-  v.regex(/^[a-zA-Z0-9.,!?;:()\-]+$/, ERR_BAD_NAME), // Matches a-z, A-Z, 0-9, and common punctuation
+  v.regex(/^[a-zA-Z0-9.,!?;:()\-]+$/, ERR_BAD_NAME) // Matches a-z, A-Z, 0-9, and common punctuation
 );
 
-const img = v.pipe(v.string(), v.url());
+const img = v.optional(v.pipe(v.string(), v.url()));
 
 const platformSchema = v.object({
   name: name,
